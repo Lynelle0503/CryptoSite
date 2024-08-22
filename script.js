a05f2dc32ab85c80bae90d826de42494a57610f2
Lynelle0503 <37027090+Lynelle0503@users.noreply.github.com>
2024-08-23 02:03:24 +0530

    files

---
+++ script.js
@@ -0,0 +1,19 @@
+
+var settings = {
+    "async":true,
+    "scrossDomain":true,
+    "url":"https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=usd",
+    "method":"GET",
+    "headers":{}
+}
+
+$.ajax(settings).done(function(response){
+    console.log(response);
+    bitcoin.innerHTML = response.bitcoin.usd;
+    ethereum.innerHTML = response.ethereum.usd;
+    dogecoin.innerHTML = response.dogecoin.usd;
+});
+
+var bitcoin = document.getElementById("bitcoin");
+var ethereum = document.getElementById("ethereum");
+var dogecoin = document.getElementById("dogecoin");
